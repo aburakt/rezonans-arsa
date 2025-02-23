e6cc615463d00bdef4bd4cd47da2226650a6a332
aburakt <info@aburakt.com>
2025-02-23 23:58:58 +0300

    feat: gradient percentage

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -16,7 +16,7 @@ export function Hero() {
         ]}
         className="h-full"
       >
-        <div className="absolute inset-0 bg-gradient-to-b from-black/60 via-black/50 to-black/60" />
+        <div className="absolute inset-0 bg-gradient-to-b from-black/20 via-black/30 to-black/30" />
 
         {/* Main content */}
         <div className="absolute inset-0 flex items-center justify-center">
